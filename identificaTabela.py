0a6f3570e655f555214c6a17544df23780b90b35
Nadia.Pontes <nadiacpontes@gmail.com>
2018-04-26 12:21:02 -0300

    lê tabela e implementa a função que faz a média pontedarada por patamar de carga

--- identificaTabela.py
+++ identificaTabela.py
@@ -3,22 +3,26 @@ pd.set_option('display.width', 400)
 
 tabela = pd.read_fwf('cmarg002.out',delim_whitespace=1, skiprows= 2)
 #o ano 2018 no arquivo cmarg vai até a linha 6009 (2000 serie, 3 patamares por serie
-print(tabela.head(6020))
-print(tabela.iloc[4,0])
-print(tabela.iloc[4,1])
-print(tabela.iloc[4,2])
-print(tabela.iloc[4,3])
-print(tabela.iloc[4,4])
-print(tabela.iloc[4,5])
-print(tabela.iloc[4,6])
-print(tabela.iloc[4,5]+tabela.iloc[4,6])
-#ano2018 = tabela.iloc[:6009,:]
-#print(ano2018)
+print(tabela.head(11))
+
 
 def mediaPatamar(pesado, medio, leve):
-    percentualPesado = 0,10
-    percentualMedio = 0,52
-    percentualLeve = 0,38
+    percentualPesado = 0.10
+    percentualMedio = 0.52
+    percentualLeve = 0.38
 
     media = pesado*percentualPesado + medio*percentualMedio + leve*percentualLeve
     return (media)
+
+inicio = 2
+fim = 4
+precoMes=[]
+for mes in range (1,14):
+    for i in range (inicio, fim+1):
+        precoMes[i] = mediaPatamar(tabela.iloc[i, mes],tabela.iloc[i+1, mes],tabela.iloc[i+2, mes])
+        i+=3
+    inicio+=1
+    fim+=1
+    mes+=1
+
+print(precoMes)
