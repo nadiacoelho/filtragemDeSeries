3eac2c241f7b7a4023006763f289f16221e62a19
Nadia.Pontes <nadiacpontes@gmail.com>
2018-04-26 12:30:03 -0300

    tentativa de media ponderada por patamar

--- identificaTabela.py
+++ identificaTabela.py
@@ -3,7 +3,7 @@ pd.set_option('display.width', 400)
 
 tabela = pd.read_fwf('cmarg002.out',delim_whitespace=1, skiprows= 2)
 #o ano 2018 no arquivo cmarg vai até a linha 6009 (2000 serie, 3 patamares por serie
-print(tabela.head(11))
+print(tabela.head(5))
 
 
 def mediaPatamar(pesado, medio, leve):
@@ -14,15 +14,15 @@ def mediaPatamar(pesado, medio, leve):
     media = pesado*percentualPesado + medio*percentualMedio + leve*percentualLeve
     return (media)
 
-inicio = 2
-fim = 4
 precoMes=[]
 for mes in range (1,14):
-    for i in range (inicio, fim+1):
-        precoMes[i] = mediaPatamar(tabela.iloc[i, mes],tabela.iloc[i+1, mes],tabela.iloc[i+2, mes])
+    for i in range (2, 15):
+        print(precoMes)
+        print('i ',i)
+        print('mes ', mes)
+        print(tabela.iloc[i,mes])
+        precoMes[mes-1] = mediaPatamar(tabela.iloc[i, mes],tabela.iloc[i+1, mes],tabela.iloc[i+2, mes])
         i+=3
-    inicio+=1
-    fim+=1
     mes+=1
 
 print(precoMes)
