0359b15daaadcb1881f3db5af7cd4e9d0eeacba7
Nadia.Pontes <nadiacpontes@gmail.com>
2018-04-27 12:19:07 -0300

    função de importação de dados melhorada função de tratamento dos dados de preço criada (trataCMO)

--- identificaTabela.py
+++ identificaTabela.py
@@ -1,28 +1,19 @@
 import pandas as pd
 pd.set_option('display.width', 400)
-
 tabela = pd.read_fwf('cmarg002.out',delim_whitespace=1, skiprows= 2)
-#o ano 2018 no arquivo cmarg vai até a linha 6009 (2000 serie, 3 patamares por serie
-print(tabela.head(5))
 
+#o ano 2018(o ano vigente) no arquivo cmarg vai até a linha 6008 (2000 serie, 3 patamares por serie + estatisticas)
+tabela2018 = tabela.iloc[:6008,:]
 
-def mediaPatamar(pesado, medio, leve):
-    percentualPesado = 0.10
-    percentualMedio = 0.52
-    percentualLeve = 0.38
+tabela2018.iloc[6002,0] = "MEDIA"
+tabela2018.iloc[6003,0] = "DESVIO PADRAO"
+tabela2018.iloc[6004,0] = "MIN"
+tabela2018.iloc[6005,0] = "P5"
+tabela2018.iloc[6006,0] = "P95"
+tabela2018.iloc[6007,0] = "MAX"
 
-    media = pesado*percentualPesado + medio*percentualMedio + leve*percentualLeve
-    return (media)
+print(tabela2018)
 
-precoMes=[]
-for mes in range (1,14):
-    for i in range (2, 15):
-        print(precoMes)
-        print('i ',i)
-        print('mes ', mes)
-        print(tabela.iloc[i,mes])
-        precoMes[mes-1] = mediaPatamar(tabela.iloc[i, mes],tabela.iloc[i+1, mes],tabela.iloc[i+2, mes])
-        i+=3
-    mes+=1
-
-print(precoMes)
+#primeiras cinco series para teste:
+#tabelaTeste = tabela.iloc[:17,:]
+#print(tabelaTeste)
---
+++ trataCMO.py
@@ -0,0 +1,93 @@
+import pandas as pd
+pd.set_option('display.width', 400)
+
+tabela1 = pd.read_fwf('cmarg001.out',delim_whitespace=1, skiprows= 2)
+tabela1_2018 = tabela1.iloc[:6002,:]
+tabela2 = pd.read_fwf('cmarg002.out',delim_whitespace=1, skiprows= 2)
+tabela2_2018 = tabela2.iloc[:6002,:]
+tabela3 = pd.read_fwf('cmarg003.out',delim_whitespace=1, skiprows= 2)
+tabela3_2018 = tabela3.iloc[:6002,:]
+tabela4 = pd.read_fwf('cmarg004.out',delim_whitespace=1, skiprows= 2)
+tabela4_2018 = tabela4.iloc[:6002,:]
+
+dfCMO1 = pd.DataFrame(columns= ["JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ", "MEDIA"])
+dfCMO2 = pd.DataFrame(columns= ["JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ", "MEDIA"])
+dfCMO3 = pd.DataFrame(columns= ["JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ", "MEDIA"])
+dfCMO4 = pd.DataFrame(columns= ["JAN", "FEV", "MAR", "ABR", "MAI", "JUN", "JUL", "AGO", "SET", "OUT", "NOV", "DEZ", "MEDIA"])
+
+def mediaPatamar(pesado, medio, leve):
+    percentualPesado = 0.10
+    percentualMedio = 0.52
+    percentualLeve = 0.38
+    media = (pesado*percentualPesado) + (medio*percentualMedio) + (leve*percentualLeve)
+    return (media)
+def defineMes(mes):
+    if mes == 1:
+        return "JAN"
+    if mes == 2:
+        return "FEV"
+    if mes == 3:
+        return "MAR"
+    if mes == 4:
+        return "ABR"
+    if mes == 5:
+        return "MAI"
+    if mes == 6:
+        return "JUN"
+    if mes == 7:
+        return "JUL"
+    if mes == 8:
+        return "AGO"
+    if mes == 9:
+        return "SET"
+    if mes == 10:
+        return "OUT"
+    if mes == 11:
+        return "NOV"
+    if mes == 12:
+        return "DEZ"
+    if mes == 13:
+        return "MEDIA"
+
+for mes in range (1,14):
+    #1 - SUDESTE
+    precoMes = []
+    i=2
+    while i < len(tabela1_2018):
+        precoMed = mediaPatamar(float(tabela1_2018.iloc[i, mes]),float(tabela1_2018.iloc[i+1, mes]),float(tabela1_2018.iloc[i+2, mes]))
+        i+=3
+        precoMes.append(precoMed)
+    label = defineMes(mes)
+    dfCMO1[label] = precoMes
+
+    # 2 - SUL
+    precoMes = []
+    i = 2
+    while i < len(tabela2_2018):
+        precoMed = mediaPatamar(float(tabela2_2018.iloc[i, mes]), float(tabela2_2018.iloc[i + 1, mes]), float(tabela2_2018.iloc[i + 2, mes]))
+        i += 3
+        precoMes.append(precoMed)
+    label = defineMes(mes)
+    dfCMO2[label] = precoMes
+
+    #1 - NORDESTE
+    precoMes = []
+    i=2
+    while i < len(tabela3_2018):
+        precoMed = mediaPatamar(float(tabela3_2018.iloc[i, mes]),float(tabela3_2018.iloc[i+1, mes]),float(tabela3_2018.iloc[i+2, mes]))
+        i+=3
+        precoMes.append(precoMed)
+    label = defineMes(mes)
+    dfCMO3[label] = precoMes
+
+    #4 - NORTE
+    precoMes = []
+    i=2
+    while i < len(tabela4_2018):
+        precoMed = mediaPatamar(float(tabela4_2018.iloc[i, mes]),float(tabela4_2018.iloc[i+1, mes]),float(tabela4_2018.iloc[i+2, mes]))
+        i+=3
+        precoMes.append(precoMed)
+    label = defineMes(mes)
+    dfCMO4[label] = precoMes
+
+print(dfCMO1)
